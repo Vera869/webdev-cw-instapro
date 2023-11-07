32eea30669c34855692ab6b0121b1f645194ef47
Vera869 <verea8910@gmail.com>
2023-11-07 20:28:44 +0700

    addPost, chacks

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+import { renderApp, setPosts } from './index.js'
+
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
 //const personalKey = "prod";
@@ -5,6 +7,7 @@ const personalKey = "Vera-Bu";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 //let token = "Bearer bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck";
+
 export function getPosts({ token }) {
   return fetch(postsHost, {
     method: "GET",
@@ -25,38 +28,10 @@ export function getPosts({ token }) {
       return response.json();
     })
     .then((data) => {
+      setPosts(data.posts)
       return data.posts;
-    })
-    // then((dataResponse) => {
-    //   const appHtml =
-    //   responseData.posts.map((post) => {
-    //     return {"id": "642bf333b959b2a4679f2e69",
-    //     "imageUrl": "https://storage.yandexcloud.net/skypro-webdev-homework-bucket/1680601903167-%25C3%2590%25C2%25A1%25C3%2590%25C2%25BD%25C3%2590%25C2%25B8%25C3%2590%25C2%25BC%25C3%2590%25C2%25BE%25C3%2590%25C2%25BA%2520%25C3%2591%25C2%258D%25C3%2590%25C2%25BA%25C3%2591%25C2%2580%25C3%2590%25C2%25B0%25C3%2590%25C2%25BD%25C3%2590%25C2%25B0%25202023-03-31%2520%25C3%2590%25C2%25B2%252012.45.42.png",
-    //     "createdAt": "2023-04-04T09:51:47.187Z",
-    //     "description": "Это я",
-    //     "user": {
-    //       "id": "642bf323b959b2a4679f2e68",
-    //       "name": "Глеб Фокин",
-    //       "login": "glebkaf777",
-    //       "imageUrl": "https://storage.yandexcloud.net/skypro-webdev-homework-bucket/1680601877737-%25C3%2590%25C2%25A1%25C3%2590%25C2%25BD%25C3%2590%25C2%25B8%25C3%2590%25C2%25BC%25C3%2590%25C2%25BE%25C3%2590%25C2%25BA%2520%25C3%2591%25C2%258D%25C3%2590%25C2%25BA%25C3%2591%25C2%2580%25C3%2590%25C2%25B0%25C3%2590%25C2%25BD%25C3%2590%25C2%25B0%25202023-03-31%2520%25C3%2590%25C2%25B2%252012.58.33.png"
-    //     },
-    //     "likes": [
-    //       { "id": "642bf323b959b2a4679f2e68", "name": "Глеб Фокин" },
-    //       { "id": "64226edb0cdb1574f162d950", "name": "Глеб Админ" },
-    //       { "id": "64255dabca1ce2a815a327d7", "name": "Глеб" }
-    //     ],
-    //     "isLiked": false
-    //   };
-    // })
-    // })
-    .catch((error) => {
-      if (error.message === "Сервер сломался") {
-        alert("Сервер сломался, попробуй позже");
-        return;
-      } if (error.message === "нет интернета") {
-        alert("Кажется, у вас сломался интернет, попробуйте позже");
-        return}
-    });
+    }).catch((error) => {
+      alert('Кажется, у вас сломался интернет, попробуйте позже')})
 }
 
 // https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
@@ -81,9 +56,9 @@ export function registerUser({ login, password, name, imageUrl }) {
           .replaceAll('"', "&quot;"),
       imageUrl,
     }),
-    headers: {
-      Authorization: token,
-    },
+    // headers: {
+    //   Authorization: token,
+    // },
   }).then((response) => {
     if(response.status === 500) {
       throw new Error("Сервер сломался");
@@ -95,8 +70,7 @@ export function registerUser({ login, password, name, imageUrl }) {
       throw new Error("Такой пользователь уже существует");
     }
     return response.json();
-  })
-  .catch((error) => {
+  }).catch((error) => {
     if (error.message === "Сервер сломался") {
       alert("Сервер сломался, попробуй позже");
       return;
@@ -156,9 +130,14 @@ export function uploadImage({ file }) {
     if(!navigator.onLine) {
       throw new Error("нет интернета")
    }
-    return response.json();
-  })
-  .catch((error) => {
+   if (response.status === 400) {
+    alert('Размер изображения превышает установленный')
+    renderApp()
+    throw new Error('Слишком большое изображение')
+    } else {
+      return response.json();
+    }
+  }).catch((error) => {
     if (error.message === "Сервер сломался") {
       alert("Сервер сломался, попробуй позже");
       return;
@@ -167,44 +146,28 @@ export function uploadImage({ file }) {
       return}
   });
 }
-// export const postFetch = () => {
-//   return fetch(postsHost, {
-//     method: "POST",
-//     headers: {
-//       Authorization: token,
-//     },
-//     body: JSON.stringify({
-//         description: post.description
-//           .replaceAll("&", "&amp;")
-//           .replaceAll("<", "&lt;")
-//           .replaceAll(">", "&gt;")
-//           .replaceAll('"', "&quot;"),
-//         imageUrl: imageUrl,
-//       }),
-//     }).then((response) => {
-//       if(response.status === 500) {
-//         throw new Error("Сервер сломался");
-//       }
-//       if(response.status === 400) {
-//         throw new Error("Плохой запрос");
-//       }
-//       if(!navigator.onLine) {
-//         throw new Error("нет интернета")
-//     }
-//       return response.json()
-//     }).then((responseData) => {
-//       return getFetch();
-//     }).then(() => {
-//     }).catch((error) => {
-//       if (error.message === "Сервер сломался") {
-//         alert("Сервер сломался, попробуй позже");
-//         return;
-//       } if (error.message === "нет интернета") {
-//         alert("Кажется, у вас сломался интернет, попробуйте позже");
-//         return
-//       }if (error.message === "Плохой запрос") {
-//           alert("Пожалуйста, проверьте правильность заполнения полей");
-//           return
-//       }
-//     });
-// }
+
+export function addPost({ token, imageUrl }) {
+  const descriptionInputElement = document.getElementById('description')
+  return fetch(postsHost, {
+    method: 'POST',
+     body: JSON.stringify({
+        description: descriptionInputElement.value
+             .replaceAll('&', '&amp;')
+            .replaceAll('<', '&lt;')
+            .replaceAll('>', '&gt;')
+             .replaceAll('"', '&quot;'),
+         imageUrl,
+     }),
+     headers: {
+         Authorization: token,
+     },
+  }).then((response) => {
+      if (response.status === 400) {
+          alert('Заполнены не все поля')
+          throw new Error('Некорректно введены данные')
+      } else {
+          return response.json()
+      }
+  })
+}
